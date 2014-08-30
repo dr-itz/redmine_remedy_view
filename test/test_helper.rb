9f2297dec53c40d2d2ac42a8578dd47a11c5cfe3
Daniel Ritz <daniel.ritz@gmx.ch>
2014-08-30 15:07:33 +0200

    Simplify fixtures loading

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -27,16 +27,12 @@ require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
 # It works like the normal #fixtures but uses the plugin's fixture instead
 module Redmine
   module PluginFixturesLoader
-    def self.included(base)
-      base.class_eval do
-        def self.plugin_fixtures(*symbols)
-          ActiveRecord::Fixtures.create_fixtures(File.dirname(__FILE__) + '/fixtures/', symbols)
-        end
-      end
+    def plugin_fixtures(*symbols)
+      ActiveRecord::Fixtures.create_fixtures(File.dirname(__FILE__) + '/fixtures/', symbols)
     end
   end
 end
 
-unless ActiveSupport::TestCase.included_modules.include?(Redmine::PluginFixturesLoader)
-  ActiveSupport::TestCase.send :include, Redmine::PluginFixturesLoader
+class ActiveSupport::TestCase
+  extend Redmine::PluginFixturesLoader
 end
