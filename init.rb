cb0b137e823db543c6f3ba8e2db6d08c3b2da441
Daniel Ritz <daniel.ritz@gmx.ch>
2015-04-03 21:55:54 +0200

    Version v0.2.0

--- init.rb
+++ init.rb
@@ -6,7 +6,7 @@ Redmine::Plugin.register :redmine_remedy_view do
   name 'Remedy View'
   author 'Daniel Ritz'
   description 'View Remedy AR tickets'
-  version '0.1.0'
+  version '0.2.0'
   url 'https://github.com/dr-itz/redmine_remedy_view '
   author_url 'https://github.com/dr-itz/'
   requires_redmine '2.2.0'
